e2ce74dcbaa3986471e3b35a89f9259f5ca8270f
hopeengineering83-code <hopeengineering83@gmail.com>
2026-06-08 16:49:51 +0300

    feat: add client submission details resolution panel

---
+++ components/client-submission-details-panel.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export function ClientSubmissionDetailsPanel({ tenderId }: { tenderId: string }) {
+  return <div data-tender-id={tenderId}>Client &amp; Submission Details</div>;
+}
